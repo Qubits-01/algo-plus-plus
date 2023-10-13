9f28f2b625f3ca7e6daf29639829f47445f34f06
Marc Peejay Viernes <mpvviernes@gmail.com>
2023-10-13 14:05:28 +0800

    A - First ABC 2
    https://atcoder.jp/contests/abc322/tasks/abc322_a

---
+++ A - First ABC 2/main.py
@@ -0,0 +1,13 @@
+# Ref:https://atcoder.jp/contests/abc322/tasks/abc322_a
+
+N: int = int(input().strip())
+S: str = input().strip()
+
+for i in range(N - 2):
+    if S[i] == "A":
+        if S[i + 1] == "B":
+            if S[i + 2] == "C":
+                print(i + 1)
+                exit()
+
+print("-1")
